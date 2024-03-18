dd45bbf0ee1ce4fce0b5e96939d905ef4dc54881
Shad02w <alvistse123@gmail.com>
2024-03-17 23:43:51 -0400

    refactor: 1. task cahce

--- app/web/src/App.tsx
+++ app/web/src/App.tsx
@@ -2,7 +2,7 @@ import { Counter } from "ui/Counter"
 
 function App() {
     return (
-        <main className="h-screen bg-red-400">
+        <main className="h-screen bg-zinc-100 flex justify-center items-center">
             <Counter />
         </main>
     )
--- lib/ui/Counter.tsx
+++ lib/ui/Counter.tsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 export function Counter() {
     const [count, setCount] = useState(0)
     return (
-        <button className="bg-black" onClick={() => setCount(_ => _ + 1)}>
+        <button className="bg-black rounded" onClick={() => setCount(_ => _ + 1)}>
             add count: {count}
         </button>
     )
