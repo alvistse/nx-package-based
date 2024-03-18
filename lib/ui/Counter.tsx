afc549d932ec1593a2e8fd6d7dc75aa384f7fd02
Shad02w <alvistse123@gmail.com>
2024-03-17 22:38:54 -0400

    refactor: setup ui lib

--- lib/ui/Counter.tsx
+++ lib/ui/Counter.tsx
@@ -2,5 +2,9 @@ import { useState } from "react"
 
 export function Counter() {
     const [count, setCount] = useState(0)
-    return <button onClick={() => setCount(_ => _ + 1)}>add count: {count}</button>
+    return (
+        <button className="bg-black" onClick={() => setCount(_ => _ + 1)}>
+            add count: {count}
+        </button>
+    )
 }
